df8944ed6c81bd0e9a075ce250fee23126870a94
pescel <28l.pesce@gmail.com>
2017-01-19 20:13:43 -0700

    vids 5-8, setting up redux, reducers

---
+++ learn-redux/client/reducers/comments.js
@@ -0,0 +1,7 @@
+
+
+function comments(state = [], action){
+  return state;
+}
+
+export default comments;
--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -11,14 +11,18 @@ import PhotoGrid from './components/PhotoGrid';
 
 // import react router dependencies
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import {Provider} from 'react-redux';
+import store, {history} from './store';
 
 const router = (
-  <Router history={browserHistory}>
+  <Provider>
+  <Router history={history}>
     <Route path="/" component={Main}>
       <IndexRoute component={PhotoGrid}></IndexRoute>
       <Route path="/view/:postId"component={Single}></Route>
     </Route>
   </Router>
+  </Provider>
 )
 
 render(router, document.getElementById('root'));
