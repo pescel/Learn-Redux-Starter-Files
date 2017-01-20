56bd550bc15b67d50ee07f8eea7474efc8d730cb
pescel <28l.pesce@gmail.com>
2017-01-19 19:16:49 -0700

    video 4, router component

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+//import css stuff
 import css from './styles/style.styl';
 
+//import the components
 import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
 
-render(<Main />, document.getElementById('root'));
+// import react router dependencies
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId"component={Single}></Route>
+    </Route>
+  </Router>
+)
+
+render(router, document.getElementById('root'));
